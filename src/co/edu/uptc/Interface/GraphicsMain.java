6c32de968a4f8e12c852b30ec5c8ed9696da7495
alexander carrillo martinez <alexandercarrillomartinez8@gmail.com>
2023-02-20 22:01:08 -0500

    Avance 20-02-2023 taller

--- src/co/edu/uptc/Interface/GraphicsMain.java
+++ src/co/edu/uptc/Interface/GraphicsMain.java
@@ -3,6 +3,7 @@ package co.edu.uptc.Interface;
 import co.edu.uptc.Logic.LinkedDouble;
 import co.edu.uptc.Logic.Node;
 import co.edu.uptc.Model.VideoBeam;
+import org.w3c.dom.xpath.XPathNamespace;
 
 import javax.swing.*;
 import java.awt.*;
@@ -18,6 +19,16 @@ public class GraphicsMain {
     JTextField tfId, tfBrand, tfModel, tfSize;
     JComboBox CBAmbientLigth, CBAutoFocus;
     Vector<Boolean> vector = new Vector<Boolean>();
+
+    JRadioButton radioButtonAddFirst;
+    JRadioButton radioButtonAddLast ;
+    JRadioButton radioButtonAddBeforeTo;
+    JRadioButton radioButtonAddAfterTo;
+    JRadioButton radioButtonFindNode;
+    JRadioButton radioButtonGetList;
+    JRadioButton radioButtonGetsize;
+    JRadioButton radioButtonGetObjetc;
+    ButtonGroup buttonGroup ;
 
     LinkedDouble<VideoBeam> ld = new LinkedDouble<>((o1, o2)-> Integer.compare(o1.getId(), o2.getId()));
 
@@ -60,12 +71,12 @@ public class GraphicsMain {
 
 
         frame.setVisible(true);
-        frame.setSize(500, 500);
+        frame.setSize(600, 500);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
 
-        panel.setBounds(5, 5, 475, 450);
+        panel.setBounds(5, 5, 575, 450);
         panel.setBackground(Color.decode("#9C9C9C"));
         panel.setLayout(null);
 
@@ -87,12 +98,63 @@ public class GraphicsMain {
         jlAutoFocus.setBounds(30, 250, 100, 30 );
         CBAutoFocus.setBounds(150, 250, 120, 30);
 
+        radioButtonAddFirst = new JRadioButton("Add First", false);
+        radioButtonAddFirst.setBounds(330, 50, 100, 30);
+        radioButtonAddFirst.setBackground(Color.decode("#9C9C9C"));
+        radioButtonAddFirst.setFocusable(false);
+
+        radioButtonAddLast = new JRadioButton("Add Last", false);
+        radioButtonAddLast.setBounds(440, 50, 100, 30);
+        radioButtonAddLast.setBackground(Color.decode("#9C9C9C"));
+        radioButtonAddLast.setFocusable(false);
+
+        radioButtonAddBeforeTo = new JRadioButton("Add Before To", false);
+        radioButtonAddBeforeTo.setBounds(330, 90, 100, 30);
+        radioButtonAddBeforeTo.setBackground(Color.decode("#9C9C9C"));
+        radioButtonAddBeforeTo.setFocusable(false);
+
+        radioButtonAddAfterTo = new JRadioButton("Add Afer To", false);
+        radioButtonAddAfterTo.setBounds(440,90, 100, 30);
+        radioButtonAddAfterTo.setBackground(Color.decode("#9C9C9C"));
+        radioButtonAddAfterTo.setFocusable(false);
+
+        radioButtonFindNode = new JRadioButton("Find Node", false);
+        radioButtonFindNode.setBounds(330,130,100,30);
+        radioButtonFindNode.setBackground(Color.decode("#9C9C9C"));
+        radioButtonFindNode.setFocusable(false);
+
+        radioButtonGetList = new JRadioButton("Get List", false);
+        radioButtonGetList.setBounds(440, 130, 100, 30);
+        radioButtonGetList.setBackground(Color.decode("#9C9C9C"));
+        radioButtonGetList.setFocusable(false);
+
+        radioButtonGetsize = new JRadioButton("Get Size", false);
+        radioButtonGetsize.setBounds(330, 170, 100, 30);
+        radioButtonGetsize.setBackground(Color.decode("#9C9C9C"));
+        radioButtonGetsize.setFocusable(false);
+
+        radioButtonGetObjetc = new JRadioButton("Get Object", false);
+        radioButtonGetObjetc.setBounds(440, 170, 100, 30);
+        radioButtonGetObjetc.setBackground(Color.decode("#9C9C9C"));
+        radioButtonGetObjetc.setFocusable(false);
+
+        buttonGroup = new ButtonGroup();
+        buttonGroup.add(radioButtonAddFirst);
+        buttonGroup.add(radioButtonAddLast);
+        buttonGroup.add(radioButtonAddBeforeTo);
+        buttonGroup.add(radioButtonAddAfterTo);
+        buttonGroup.add(radioButtonFindNode);
+        buttonGroup.add(radioButtonGetList);
+        buttonGroup.add(radioButtonGetsize);
+        buttonGroup.add(radioButtonGetObjetc);
 
 
         buttonCreate.setBounds(30, 400, 100, 30);
         buttonRead.setBounds(140, 400, 100,30);
         buttonUpdate.setBounds(250, 400, 100, 30);
         buttonDelete.setBounds(360, 400, 100, 30);
+
+
 
 
         frame.add(panel);
@@ -108,6 +170,14 @@ public class GraphicsMain {
         panel.add(CBAmbientLigth);
         panel.add(jlAutoFocus);
         panel.add(CBAutoFocus);
+        panel.add(radioButtonAddFirst);
+        panel.add(radioButtonAddLast);
+        panel.add(radioButtonAddBeforeTo);
+        panel.add(radioButtonAddAfterTo);
+        panel.add(radioButtonFindNode);
+        panel.add(radioButtonGetList);
+        panel.add(radioButtonGetsize);
+        panel.add(radioButtonGetObjetc);
         panel.add(buttonCreate);
         panel.add(buttonRead);
         panel.add(buttonUpdate);
--- src/co/edu/uptc/Logic/LinkedDouble.java
+++ src/co/edu/uptc/Logic/LinkedDouble.java
@@ -1,10 +1,12 @@
 package co.edu.uptc.Logic;
 
+import co.edu.uptc.Model.VideoBeam;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class LinkedDouble <T>{
+public class LinkedDouble<T> {
     private Comparator<T> comparator;
     private Node<T> head;
     private Node<T> last;
@@ -13,137 +15,129 @@ public class LinkedDouble <T>{
         this.comparator = comparator;
     }
 
-  public boolean isEmpty(){
-      return head== null && last == null;
-  }
+    public boolean isEmpty() {
+        return head == null && last == null;
+    }
 
-  public void addNodeFirst(T info){
-      if( isEmpty()){
-          head = last = new Node<>(info);
-      }else{
-          Node<T> node = new Node<>(info);
-          head.setPrevious( node );
-          node.setNext( head );
-          head = node;
-      }
-  }
+    public void addNodeFirst(T info) {
+        if (isEmpty()) {
+            head = last = new Node<>(info);
+        } else {
+            Node<T> node = new Node<>(info);
+            head.setPrevious(node);
+            node.setNext(head);
+            head = node;
+        }
+    }
 
-  public void addNodeLast(T info){
-      if( isEmpty()){
-          head = last = new Node<>(info);
-      }else{
-          Node<T> node = new Node<>(info);
-          last.setNext( node);
-          node.setPrevious(last);
-          last = node;
-      }
-  }
+    public void addNodeLast(T info) {
+        if (isEmpty()) {
+            head = last = new Node<>(info);
+        } else {
+            Node<T> node = new Node<>(info);
+            last.setNext(node);
+            node.setPrevious(last);
+            last = node;
+        }
+    }
 
-  public void addNodeBeforeTo(Node<T> before, T info){
+    public void addNodeBeforeTo(Node<T> before, T info) {
         try {
-            if (isEmpty()){
+            if (isEmpty()) {
                 head = last = new Node(info);
-            }else{
+            } else {
                 Node<T> node = new Node<>(info);
                 node.setNext(before);
                 node.setPrevious(before.getPrevious());
                 before.getPrevious().setNext(node);
                 before.setPrevious(node);
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
 
-  }
+    }
 
-  public void  addNodeAfterTo(Node<T> after, T info){
+    public void addNodeAfterTo(Node<T> after, T info) {
         try {
-            if (isEmpty()){
+            if (isEmpty()) {
                 head = last = new Node(info);
-            }else{
+            } else {
                 Node<T> node = new Node<>(info);
                 node.setNext(after.getNext());
                 after.getNext().setPrevious(node);
                 node.setPrevious(after);
                 after.setNext(node);
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
-  }
+    }
 
-  public void addNodeSorted(){
+    public void addNodeSorted() {
 
-  }
+    }
 
-  public Node<T> findNode(T node){
-      Node<T> aux = head;
-      while (aux != null) {
-          if (comparator.compare(node, aux.getInfo())==0) {
-              return aux;
-          }
-          aux = aux.getNext();
-      }
-      return null;
-  }
+    public Node<T> findNode(T node) {
+        Node<T> aux = head;
+        while (aux != null) {
+            if (comparator.compare(node, aux.getInfo()) == 0) {
+                return aux;
+            }
+            aux = aux.getNext();
+        }
+        return null;
+    }
 
-  public T findInfo(T info){
+    public T findInfo(T info) {
 
-      return info;
-  }
+        return info;
+    }
 
-  public List<T> getLinkedList(boolean back){
-      ArrayList<T> out = new ArrayList<>();
-      Node<T> aux = !back ? head : last;
-      while(aux != null){
-          out.add(aux.getInfo());          aux = !back ? aux.getNext() : aux.getPrevious();
-      }
-      return out;
-  }
+    public List<T> getLinkedList(boolean back) {
+        ArrayList<T> out = new ArrayList<>();
+        Node<T> aux = !back ? head : last;
+        while (aux != null) {
+            out.add(aux.getInfo());
+            aux = !back ? aux.getNext() : aux.getPrevious();
+        }
+        return out;
+    }
 
-  public T deleteNode(Node<T> delete){
-      if (delete == null) {
-          return null;
-      }
+    public T deleteNode(Node<T> delete) {
+        if (head == delete) {
+            head = head.getNext();
+            head.setPrevious(null);
+        } else {
+           delete.getPrevious().setNext(delete.getNext());
+           delete.getNext().setPrevious(delete.getPrevious());
+        }
+        return delete.getInfo();
+    }
 
-      if (delete == head) {
-          head = delete.getNext();
-      }
+    public int getSize() {
+        int i = 1;
+        Node<T> node = head;
+        if (isEmpty()) {
+            return 0;
+        } else {
+            while (node.getNext() != null) {
+                node = node.getNext();
+                i++;
+            }
+        }
+        return i;
+    }
 
-      if (delete == last) {
-          last = delete.getPrevious();
-      }
+    public T getObject(int pos) {
+        try {
+            return getLinkedList(false).get(pos);
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+            return null;
+        }
 
-      if (delete.getPrevious() != null) {
-        //  delete.getPrevious() = delete.getNext();
-          delete.setNext(delete.getPrevious());
-      }
-
-      if (delete.getNext() != null) {
-       //   delete.getNext().getPrevious() = delete.getPrevious();
-          delete.setPrevious(delete.getNext());
-      }
-      return null;
-  }
-
-  public int getSize(){
-      int i = 1;
-      Node<T> node = head;
-      if (isEmpty()) {
-          return 0;
-      } else {
-          while (node.getNext() != null) {
-              node = node.getNext();
-              i++;
-          }
-      }
-      return i;
-  }
-
-  public T getObject(int pos){
-
-      return null;
-  }
+    }
 }
